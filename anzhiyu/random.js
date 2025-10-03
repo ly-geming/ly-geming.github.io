3abf18bfaa1a737c6f6dd101c1c1db9f1b9a323b
ly-geming <3430650671@qq.com>
2025-10-03 15:59:44 +0800

    Site updated: 2025-10-03 15:59:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/27/史纲/","2025/09/29/杭州/","2025/09/01/国庆规划/"];function toRandomPost(){
+var posts=["2025/09/27/史纲/","2025/09/29/杭州/","2025/09/01/国庆规划/","2025/10/03/post/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
